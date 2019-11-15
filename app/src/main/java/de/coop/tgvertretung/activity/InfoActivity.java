33c81b6e0be5f99cf2a8c9ae7f227ef802ccb689
Christoph <mcteccy@gmail.com>
2019-11-15 22:51:19 +0100

    Added more actions to the info activity

--- app/src/main/java/de/coop/tgvertretung/activity/InfoActivity.java
+++ app/src/main/java/de/coop/tgvertretung/activity/InfoActivity.java
@@ -1,9 +1,11 @@
 package de.coop.tgvertretung.activity;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
@@ -12,6 +14,10 @@ import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import de.coop.tgvertretung.BuildConfig;
 import de.coop.tgvertretung.R;
 
 public class InfoActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
@@ -37,6 +43,15 @@ public class InfoActivity extends AppCompatActivity implements DialogInterface.O
             startActivity(browserIntent);
         });
 
+        LinearLayout developers = findViewById(R.id.developers);
+        developers.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.devs_url)));
+            startActivity(browserIntent);
+        });
+
+        LinearLayout version = findViewById(R.id.version);
+        version.setOnClickListener(v -> makeVersionDialog());
+
         LinearLayout licenses = findViewById(R.id.licenses);
         licenses.setOnClickListener(v -> makeLicenseDialog());
     }
@@ -53,6 +68,20 @@ public class InfoActivity extends AppCompatActivity implements DialogInterface.O
         builder.setTitle(R.string.licenses_name);
         builder.setItems(R.array.licenses, this);
         builder.show();
+    }
+
+    private void makeVersionDialog(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_about_version);
+        dialog.setTitle(R.string.version);
+        TextView versionName = dialog.findViewById(R.id.version_name);
+        TextView versionID = dialog.findViewById(R.id.version_id);
+        TextView buildDate = dialog.findViewById(R.id.build_date);
+
+        versionName.setText(getVersion());
+        versionID.setText(getVersionID());
+        buildDate.setText(getBuildDate());
+        dialog.show();
     }
 
     @Override
@@ -73,5 +102,23 @@ public class InfoActivity extends AppCompatActivity implements DialogInterface.O
             e.printStackTrace();
             return null;
         }
+    }
+
+    private String getVersionID(){
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+                return String.valueOf(getPackageManager().getPackageInfo(getPackageName(), 0).getLongVersionCode());
+            return String.valueOf(getPackageManager().getPackageInfo(getPackageName(), 0).versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getBuildDate(){
+        String s;
+        DateFormat format = DateFormat.getDateInstance();
+        s = format.format(new Date(BuildConfig.TIMESTAMP));
+        return s;
     }
 }
