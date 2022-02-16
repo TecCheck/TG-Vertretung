d83c82383ed0d5a9c72e1200c710640c419aff88
TecCheck <mcteccy@protonmail.com>
2022-02-16 11:51:37 +0100

    Clean up MainActivity

--- app/src/main/java/de/coop/tgvertretung/activity/MainActivity.java
+++ app/src/main/java/de/coop/tgvertretung/activity/MainActivity.java
@@ -10,21 +10,20 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 
 import de.coop.tgvertretung.R;
@@ -34,17 +33,16 @@ import de.coop.tgvertretung.utils.Downloader;
 import de.coop.tgvertretung.utils.Settings;
 import de.coop.tgvertretung.utils.SettingsWrapper;
 import de.coop.tgvertretung.utils.Utils;
+import de.sematre.tg.Table;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, Downloader.LoadFinishedListener {
 
-    private static boolean firstPagerStart = true;
+    private ViewPager pager;
     private DrawerLayout drawer;
-    private ViewPager mPager = null;
-    private TextView lastReload = null;
-    private TextView lastServerRefresh = null;
-    private SwipeRefreshLayout refreshLayout = null;
+    private SwipeRefreshLayout refreshLayout;
+
     private SettingsWrapper settings;
-    SettingsWrapper.SettingsWriter settingsWriter;
+    private SettingsWrapper.SettingsWriter settingsWriter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,98 +54,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             return;
         }
 
-        settingsWriter = new SettingsWrapper.SettingsWriter(this);
         setContentView(R.layout.activity_main);
-        initUi();
-        startPagerView();
 
-        load();
-        if (!BackgroundService.isRunning) {
-            startService(new Intent(getApplicationContext(), BackgroundService.class));
-        }
+        Settings.load(this);
+        settingsWriter = new SettingsWrapper.SettingsWriter(this);
 
-        new File(Utils.getUpdateDownloadFile(this)).delete();
-        Utils.print(Utils.getUpdateDownloadFile(this));
-    }
-
-    private void startLoginActivity(boolean reLogin) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.putExtra(LoginActivity.EXTRA_RE_LOGIN, reLogin);
-        if (!reLogin)
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-    private void initUi() {
-        int themeMode = settings.getThemeMode();
-        if (themeMode == 0 && Build.VERSION.SDK_INT >= 29)
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        else
-            AppCompatDelegate.setDefaultNightMode(themeMode);
-
+        refreshLayout = findViewById(R.id.refresh_layout);
+        drawer = findViewById(R.id.drawer_layout);
+        pager = findViewById(R.id.container);
         Toolbar toolbar = findViewById(R.id.toolbar);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
-        drawer = findViewById(R.id.drawer_layout);
-        mPager = findViewById(R.id.container);
-        refreshLayout = findViewById(R.id.refresh_layout);
-
-        // Navigation Drawer
+        setNightMode(settings.getThemeMode());
         setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @Override
             public void onDrawerOpened(View drawerView) {
-                lastReload = findViewById(R.id.last_reload);
-                lastServerRefresh = findViewById(R.id.last_reload2);
-
-                TextView title1 = findViewById(R.id.textView5);
-                TextView title2 = findViewById(R.id.textView6);
+                TextView lastClientReload = findViewById(R.id.lastClientReload);
+                TextView lastServerReload = findViewById(R.id.lastServerReload);
 
                 try {
-                    if (settings.getShowClientRefresh()) {
-                        lastReload.setVisibility(View.VISIBLE);
-                        title1.setVisibility(View.VISIBLE);
-                        showTime(lastServerRefresh, new Date(settings.getLastClientRefresh()), settings.getShowRelativeTime());
-                    } else {
-                        lastReload.setVisibility(View.GONE);
-                        title1.setVisibility(View.GONE);
-                    }
-
-                    if (settings.getShowServerRefresh()) {
-                        lastServerRefresh.setVisibility(View.VISIBLE);
-                        title2.setVisibility(View.VISIBLE);
-                        showTime(lastServerRefresh, Settings.settings.timeTable.getDate(), settings.getShowRelativeTime());
-                    } else {
-                        lastServerRefresh.setVisibility(View.GONE);
-                        title2.setVisibility(View.GONE);
-                    }
+                    boolean relativeTime = settings.getShowRelativeTime();
+                    showTime(lastClientReload, new Date(settings.getLastClientRefresh()), R.string.last_client_refresh, relativeTime, settings.getShowClientRefresh());
+                    showTime(lastServerReload, Settings.settings.timeTable.getDate(), R.string.last_server_refresh, relativeTime, settings.getShowServerRefresh());
                 } catch (Exception e) {
                     e.printStackTrace();
-                    lastReload.setText(getString(R.string.last_reload_none));
-                    lastServerRefresh.setText(getString(R.string.last_reload_none));
+                    lastClientReload.setText(getString(R.string.last_reload_none));
+                    lastServerReload.setText(getString(R.string.last_reload_none));
                 }
-
                 super.onDrawerOpened(drawerView);
             }
         };
         drawer.addDrawerListener(toggle);
-        navigationView.getMenu().getItem(0).setChecked(true);
-
-        navigationView.setItemTextColor(AppCompatResources.getColorStateList(this, R.color.nav_drawer_text));
-        navigationView.setItemIconTintList(AppCompatResources.getColorStateList(this, R.color.nav_drawer_icon));
         toggle.syncState();
 
+        navigationView.getMenu().getItem(0).setChecked(true);
+        navigationView.setItemTextColor(AppCompatResources.getColorStateList(this, R.color.nav_drawer_text));
+        navigationView.setItemIconTintList(AppCompatResources.getColorStateList(this, R.color.nav_drawer_icon));
         navigationView.setNavigationItemSelectedListener(this);
 
-        refreshLayout.setOnRefreshListener(this::load);
-        try {
-            refreshLayout.setColorSchemeColors(Utils.getColor(getApplicationContext(), Settings.settings.timeTable.getTables().get(0).getDate()));
-        } catch (IndexOutOfBoundsException ignored) {
-        }
-
-        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
+        pager.setAdapter(new ScreenSlidePagerAdapter(getSupportFragmentManager(), settings));
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int i, float v, int i1) {
             }
@@ -161,54 +109,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 refreshLayout.setColorSchemeColors(Utils.getColor(getApplicationContext(), Settings.settings.timeTable.getTables().get(i).getDate()));
             }
         });
-    }
 
-    private void showTime(TextView textView, Date date, boolean relativeTime) {
-        if (relativeTime)
-            textView.setText(Utils.getRelativeFormattedTime(date, new Date(System.currentTimeMillis())));
-        else
-            textView.setText(Utils.getFormattedDate(date, false, true));
-    }
+        setPage(Utils.getView(Settings.settings.timeTable));
+        refreshLayout.setOnRefreshListener(this::load);
 
-    private void startPagerView() {
-        int index = Utils.getView(Settings.settings.timeTable);
-        if (mPager != null) index = mPager.getCurrentItem();
-
-        PagerAdapter mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), settings);
-        mPager = findViewById(R.id.container);
-        mPager.setAdapter(mPagerAdapter);
-        if (index >= 0) mPager.setCurrentItem(index);
-    }
-
-    private void load() {
-        boolean success = Downloader.download(this, settings);
-        refreshLayout.setRefreshing(success);
-    }
-
-    @Override
-    public void loadFinished(int status) {
-        if (status != 1) settingsWriter.setLastClientRefresh(System.currentTimeMillis());
-        settingsWriter.writeEditsAsync();
-
-        refreshLayout.setRefreshing(false);
-
-        if (status == 0) {
-            showSnack(getString(R.string.connected));
-        } else if (status == 1) {
-            showSnack(getString(R.string.no_connection));
-        } else if (status == 2) {
-            showSnack(getString(R.string.nothing_new));
-        }
-
-        int currentIndex = mPager.getCurrentItem();
-        startPagerView();
-
-        if (firstPagerStart) {
-            setTable(Utils.getView(Settings.settings.timeTable));
-            firstPagerStart = false;
-        } else {
-            setTable(currentIndex);
-        }
+        load();
+        startBackgroundService();
     }
 
     @Override
@@ -240,11 +146,75 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
     }
 
-    private void setTable(int index) {
-        if (index >= 0) mPager.setCurrentItem(index);
+    @Override
+    public void loadFinished(int status) {
+        if (status != 1)
+            settingsWriter.setLastClientRefresh(System.currentTimeMillis());
+        settingsWriter.writeEditsAsync();
+
+        refreshLayout.setRefreshing(false);
+
+        if (status == 0) {
+            showSnack(getString(R.string.connected));
+        } else if (status == 1) {
+            showSnack(getString(R.string.no_connection));
+        } else if (status == 2) {
+            showSnack(getString(R.string.nothing_new));
+        }
+
+        pager.getAdapter().notifyDataSetChanged();
+    }
+
+    private void startLoginActivity(boolean reLogin) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(LoginActivity.EXTRA_RE_LOGIN, reLogin);
+        if (!reLogin)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void setNightMode(int mode) {
+        if (mode == 0 && Build.VERSION.SDK_INT >= 29)
+            mode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+
+        AppCompatDelegate.setDefaultNightMode(mode);
+    }
+
+    private void showTime(TextView textView, Date date, int resId, boolean relativeTime, boolean visible) {
+        textView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        if (!visible)
+            return;
+
+        String time;
+        if (relativeTime)
+            time = Utils.getRelativeFormattedTime(date, new Date(System.currentTimeMillis()));
+        else
+            time = Utils.getFormattedDate(date, false, true);
+
+        textView.setText(getString(resId, time));
+    }
+
+    private void startBackgroundService() {
+        // TODO: Better implementation
+        if (!BackgroundService.isRunning) {
+            startService(new Intent(getApplicationContext(), BackgroundService.class));
+        }
+    }
+
+    private void load() {
+        refreshLayout.setRefreshing(Downloader.download(this, settings));
+    }
+
+    private void setPage(int index) {
+        ArrayList<Table> tables = Settings.settings.timeTable.getTables();
+        if (0 > index || index >= tables.size())
+            return;
+
+        pager.setCurrentItem(index);
+        refreshLayout.setColorSchemeColors(Utils.getColor(this, tables.get(index).getDate()));
     }
 
     private void showSnack(String text) {
-        Snackbar.make(mPager, text, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        Snackbar.make(pager, text, Snackbar.LENGTH_LONG).setAction("Action", null).show();
     }
 }
